f7b84995c9c84709858059ee0daf00a0919f515f
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-10 20:56:10 +0100

    Update:
    - Added Delete Functionality
    - Splited the components

--- src/components/CountriesTable.jsx
+++ src/components/CountriesTable.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CountryTable from "./countryTable";
 import TopNavigation from "./TopNavigation";
 
 export default function CountriesTable() {
@@ -36,6 +37,11 @@ export default function CountriesTable() {
     setCountries(Countries);
   };
 
+  const deleteCountry = (id) => {
+    const Countries = countries.filter((el, i) => i != id);
+    setCountries(Countries);
+  };
+
   return (
     <main className="container mx-auto px-4">
       <TopNavigation />
@@ -53,60 +59,12 @@ export default function CountriesTable() {
 
         <div className="table-row-group">
           {countries.map((country, i) => (
-            <div className="text-sm table-row">
-              <div className="border-t p-2 table-cell">{i + 1}</div>
-              <div className="border-t p-2 table-cell">{country.name}</div>
-              <div className="border-t p-2 table-cell">{country.alias}</div>
-              <div className="border-t p-2 table-cell">{country.captain}</div>
-              <div className="border-t p-2 table-cell">
-                <div className="flex space-x-4 items-center justify-end">
-                  <button className="rounded h-full p-2 hover:bg-gray-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                      />
-                    </svg>
-                  </button>
-                  <button
-                    className="rounded h-full p-2 hover:bg-gray-100"
-                    onClick={() => {
-                      editCountry(i);
-                    }}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                      />
-                    </svg>
-                  </button>
-
-                  <label className="rounded flex h-full p-2 hover:bg-gray-100 ">
-                    <input
-                      type="checkbox"
-                      className="p-1 accent-gray-900 hover:accent-red-700"
-                    />
-                  </label>
-                </div>
-              </div>
-            </div>
+            <CountryTable
+              index={i}
+              data={country}
+              editCountry={editCountry}
+              deleteCountry={deleteCountry}
+            />
           ))}
         </div>
       </section>
---
+++ src/components/countryTable.jsx
@@ -0,0 +1,63 @@
+export default function CountryTable({
+  index,
+  data,
+  editCountry,
+  deleteCountry,
+}) {
+  return (
+    <div className="text-sm table-row">
+      <div className="border-t p-2 table-cell">{index + 1}</div>
+      <div className="border-t p-2 table-cell">{data.name}</div>
+      <div className="border-t p-2 table-cell">{data.alias}</div>
+      <div className="border-t p-2 table-cell">{data.captain}</div>
+      <div className="border-t p-2 table-cell">
+        <div className="flex space-x-4 items-center justify-end">
+          <button className="rounded h-full p-2 hover:bg-gray-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+              />
+            </svg>
+          </button>
+          <button
+            className="rounded h-full p-2 hover:bg-gray-100"
+            onClick={() => {
+              editCountry(index);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+          </button>
+
+          <label className="rounded flex h-full p-2 hover:bg-gray-100 ">
+            <input
+              type="checkbox"
+              className="p-1 accent-gray-900 hover:accent-red-700"
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
